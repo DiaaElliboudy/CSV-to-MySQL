d45afbb53fc0caa4366484b407c67227d7a41b24
DiaaElliboudy <diaaelliboudy@gmail.com>
2024-12-12 21:32:26 +0200

    Add files via upload

---
+++ CSV_to_MySQL.py
@@ -0,0 +1,20 @@
+import pymysql
+import csv
+
+host = "localhost"
+user = "root"
+password = ""
+db = "assessment"
+
+database = pymysql.connect(host=host, user=user, passwd=password, db=db)
+cursor = database.cursor()
+csv_data = csv.reader(open("C:\\Users\\diaae\\Downloads\\countries_visa_free_access.csv"))
+header = next(csv_data)
+print("File is being imported")
+for row in csv_data:
+    print(row)
+    cursor.execute("INSERT INTO visafreeaccess (Country, Rank, VisaFreeAccess) VALUES (%s, %s, %s)",row)
+
+database.commit()
+cursor.close()
+print("CSV File Loaded in Database Successfully")
